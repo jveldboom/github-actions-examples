1942c01797dffae03f29de0d4c73834ee39e12b4
John Veldboom <john_veldboom@discovery.com>
2021-01-22 21:24:27 -0500

    update branch name for prs

--- .github/actions/setup/index.js
+++ .github/actions/setup/index.js
@@ -4,7 +4,9 @@ const github = require('@actions/github')
 try {
   const payload = github.context.payload
 
-  core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').slice(2).join('/'))
+  core.exportVariable('BRANCH_NAME', github.ref.substr(11))
+  if(github.pull_request) core.exportVariable('BRANCH_NAME', payload.head.ref)
+
   core.exportVariable('DEFAULT_BRANCH', payload.repository.default_branch)
 
   console.log(JSON.stringify(github, undefined, 2))
